8f2447cdc35dc5d750b10b4f3dd346ae8cf70d45
HenryCapdevilla <henrydaniel2002@hotmail.com>
2023-08-31 09:58:43 -0500

    git remote add origin https://github.com/HenryCapdevilla/TaxyMonitor.git

--- app/src/main/java/com/example/gps_services/MainActivity.java
+++ app/src/main/java/com/example/gps_services/MainActivity.java
@@ -25,16 +25,21 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_FINE_LOCATION = 99;
+    private static final int PERMISSION_BACKGROUND_LOCATION = 100;
     private TextView tv_lbladdress;
     private Button Btn_Get_Location;
-    private Button tcpButton;
     private Button udpButton;
+    private Button StopButton;
     private EditText ipAddressEditText;
     private EditText Input_Port;
     private String ipAddress;
@@ -55,6 +60,27 @@ public class MainActivity extends AppCompatActivity {
         tv_lbladdress = findViewById(R.id.tv_lbladdress);
         ipAddressEditText = findViewById(R.id.ipAddressEditText);
         updateGPS();
+
+        // Actualiza los permisos al iniciar la actividad
+        updatePermissions();
+    }
+
+    private void updatePermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permiso no concedido, solicitarlo
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION},
+                        PERMISSION_BACKGROUND_LOCATION);
+            } else {
+                // Permiso ya concedido o versión de Android anterior a Q
+                updateGPS();
+            }
+        } else {
+            // En versiones anteriores a Android Q, no es necesario el permiso de fondo
+            updateGPS();
+        }
     }
 
     @Override
@@ -67,6 +93,13 @@ public class MainActivity extends AppCompatActivity {
                 else{
                     Toast.makeText(this, "Esta aplicación requiere de permiso concedido para trabajar con propiedades ", Toast.LENGTH_SHORT).show();
                     finish();
+                }
+                break;
+            case PERMISSION_BACKGROUND_LOCATION: // Nuevo código
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                } else {
+                    Toast.makeText(this, "Esta aplicación requiere de permiso de segundo plano, activalo en ajustes ", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
@@ -90,9 +123,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             //El usuario no da permiso aún :(
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
-            }
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_FINE_LOCATION);
         }
     }
 
@@ -109,12 +140,22 @@ public class MainActivity extends AppCompatActivity {
                 double latitude = location.getLatitude();
                 double longitude = location.getLongitude();
                 double altitude = location.getAltitude();
+
                 double time = location.getTime();
                 Date date = new Date((long) time);  // Convertir el valor de tiempo a una instancia de Date
-                String formattedDate = date.toString();
+
+                // Crear un formato para la fecha y la hora
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+
+                String formattedDate = dateFormat.format(date); // Obtener la fecha formateada
+                String formattedTime = timeFormat.format(date); // Obtener la hora formateada
+
+                // Ahora tienes la fecha y la hora en las variables formattedDate y formattedTime
+
 
                 // Concatenar la latitud y longitud en una cadena de texto
-                String latLngAltText = "Lat: " + latitude + ", Lon: " + longitude + ", Alt: " + altitude + ", Time Stamp: " + formattedDate;
+                String latLngAltText = latitude +", " + longitude + ", " + formattedDate + ", " +formattedTime;
 
                 // Actualizar el cuadro de texto con la cadena resultante
                 tv_lbladdress.setText(latLngAltText);
@@ -124,17 +165,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void Send_Gps_To_Ip() {
-
-        tcpButton = findViewById(R.id.tcpButton);
         udpButton = findViewById(R.id.udpButton);
         Input_Port = findViewById(R.id.Input_Port);
-
-        tcpButton.setOnClickListener(v -> {
-            ipAddress = ipAddressEditText.getText().toString();
-            Port = Input_Port.getText().toString();
-            //Toast.makeText(MainActivity.this, "Botón TCP funciona", Toast.LENGTH_SHORT).show();
-            sendCoordinatesUsingTCP(ipAddress, Port);
-        });
 
         udpButton.setOnClickListener(v -> {
             ipAddress = ipAddressEditText.getText().toString();
@@ -143,28 +175,12 @@ public class MainActivity extends AppCompatActivity {
             sendCoordinatesUsingUDP(ipAddress, Port);
         });
     }
-    private void sendCoordinatesUsingTCP(final String ipAddress, String port) {
-        new Thread(() -> {
-            try {
-                Socket socket = new Socket(ipAddress, Integer.parseInt(port));
-                OutputStream outputStream = socket.getOutputStream();
-                String message = "Cordenadas: " + tv_lbladdress.getText().toString();
-                outputStream.write(message.getBytes());
-                outputStream.flush();
-                socket.close();
-                runOnUiThread(() -> Toast.makeText(MainActivity.this, "Coordenadas enviadas por TCP", Toast.LENGTH_SHORT).show());
-
-                } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
 
     private void sendCoordinatesUsingUDP(final String ipAddress, String port) {
         new Thread(() -> {
             try {
                 DatagramSocket socket = new DatagramSocket();
-                byte[] data = ("Cordenadas" + tv_lbladdress.getText().toString()).getBytes();
+                byte[] data = (tv_lbladdress.getText().toString()).getBytes();
                 DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(ipAddress), Integer.parseInt(port));
                 socket.send(packet);
                 socket.close();
